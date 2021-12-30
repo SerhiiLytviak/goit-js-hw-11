30de4c60146c6721d217022072e3f4a2767a9d90
SerhiiLytviak <litvyak.sergey@gmail.com>
2021-12-30 15:49:07 +0200

    Update index.js
    Избавился от дублирующегося кода созданием функции fetchGallery

--- src/index.js
+++ src/index.js
@@ -23,46 +23,19 @@ refs.loadMoreBtn.classList.add('is-hidden');
 
 function onSubmitClick(e) {
   e.preventDefault();
+
   refs.loadMoreBtn.classList.add('is-hidden');
+
   const form = e.currentTarget;
   pixabayFetch.query = form.elements.searchQuery.value;
+
   refs.gallery.innerHTML = '';
   pixabayFetch.resetPage();
-  pixabayFetch
-    .fetchSearchQuery()
-    .then(result => {
-      console.log(result);
-      if (result.length === 40) {
-        refs.loadMoreBtn.classList.remove('is-hidden');
-      }
-      if (result.length < 40) {
-        refs.loadMoreBtn.classList.add('is-hidden');
-        Notify.warning("We're sorry, but you've reached the end of search results.");
-      }
-      renderGalleryItem(result);
-      lightbox.refresh();
-    })
-    // .then(() => )
-    .catch(error => console.log(error));
+  fetchGallery();
 }
 
 function onLoadMore() {
-  pixabayFetch
-    .fetchSearchQuery()
-    .then(result => {
-      console.log(result);
-      if (result.length === 40) {
-        refs.loadMoreBtn.classList.remove('is-hidden');
-      }
-      if (result.length < 40) {
-        refs.loadMoreBtn.classList.add('is-hidden');
-        Notify.warning("We're sorry, but you've reached the end of search results.");
-      }
-      renderGalleryItem(result);
-      lightbox.refresh();
-    })
-    // .then(() => lightbox.refresh())
-    .catch(error => console.log(error));
+  fetchGallery();
 }
 
 function renderGalleryItem(item) {
@@ -71,36 +44,19 @@ function renderGalleryItem(item) {
   });
 }
 
-// function renderGalleryItem(item) {
-//   item.map(item => {
-//     return refs.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(item));
-//   });
-// }
-
-// function createGalleryMarkup(item) {
-//   return item
-//     .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
-//       return `
-//         <a href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" width="325" height="220" /></a>
-//       <div class="info">
-//           <p class="info-item">
-//               <b>Likes</b>
-//               ${likes}
-//           </p>
-//           <p class="info-item">
-//               <b>Views</b>
-//               ${views}
-//           </p>
-//           <p class="info-item">
-//               <b>Comments</b>
-//               ${comments}
-//           </p>
-//           <p class="info-item">
-//               <b>Downloads</b>
-//               ${downloads}
-//           </p>
-//       </div>
-//     `;
-//     })
-//     .join('');
-// }
+function fetchGallery() {
+  pixabayFetch
+    .fetchSearchQuery()
+    .then(result => {
+      if (result.length === 40) {
+        refs.loadMoreBtn.classList.remove('is-hidden');
+      }
+      if (result.length < 40) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notify.warning("We're sorry, but you've reached the end of search results.");
+      }
+      renderGalleryItem(result);
+      lightbox.refresh();
+    })
+    .catch(error => console.log(error));
+}
